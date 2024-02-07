7df8f3e6afbb1fe19f7f14b2d42a853d52f503ee
Hamdy Youssef <hamdyyoussef962@gmail.com>
2024-02-07 19:43:29 +0200

    Connect 4

---
+++ ConnectFour/BoardCell.swift
@@ -0,0 +1,13 @@
+//
+//  BoardCell.swift
+//  ConnectFour
+//
+//  Created by Hamdy Youssef on 06/02/2024.
+//
+
+import UIKit
+
+class BoardCell: UICollectionViewCell {
+     
+    @IBOutlet var image: UIImageView!
+}
---
+++ ConnectFour/BoardItem.swift
@@ -0,0 +1,42 @@
+//
+//  BoardItem.swift
+//  ConnectFour
+//
+//  Created by Hamdy Youssef on 06/02/2024.
+//
+
+import Foundation
+import UIKit
+
+enum Tile {
+    case Red
+    case Yellow
+    case Empty
+}
+
+struct BoardItem {
+    var indexPath: IndexPath!
+    var tile: Tile!
+    
+    func yellowTile() -> Bool {
+        return tile == Tile.Yellow
+    }
+    
+    func redTile() -> Bool {
+        return tile == Tile.Red
+    }
+    
+    func emptyTile() -> Bool {
+        return tile == Tile.Empty
+    }
+    
+    func tileColor() -> UIColor {
+        if redTile() {
+            return .red
+        }
+        if yellowTile() {
+            return .yellow
+        }
+        return .white 
+    }
+}
